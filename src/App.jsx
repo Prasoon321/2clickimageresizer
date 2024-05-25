6882232eb51436f3d895e325167260c4a83863e5
prasoon <jbprasoon@gmail.com>
2024-05-25 10:22:59 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "../css/Footer.module.css";
+function Footer() {
+  return (
+    <>
+      <footer className={styles.main}>
+        <h1>Made By Prasoon Senagr</h1>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
